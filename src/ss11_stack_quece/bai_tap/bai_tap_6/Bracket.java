71d8b66484ddd19412a839e009e71420dfc43764
pham-van-man <phamvanman.com.vn@gmail.com>
2024-07-17 09:05:15 +0700

    first commit

--- src/ss11_stack_quece/bai_tap/bai_tap_6/Bracket.java
+++ src/ss11_stack_quece/bai_tap/bai_tap_6/Bracket.java
@@ -5,6 +5,27 @@ import java.util.Stack;
 
 public class Bracket {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>();
+        Stack<String> bracketStack = new Stack<>();
+        String expression = "s * (s – a) * (s – b) * (s – c)";
+        System.out.println(checkBracket(expression, bracketStack));
+    }
+
+    private static boolean checkBracket(String expression, Stack<String> bracketStack) {
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '(') {
+                bracketStack.push("(");
+            } else if (ch == ')') {
+                if (bracketStack.isEmpty()) {
+                    return false;
+                } else {
+                    String leftBracket = bracketStack.pop();
+                    if (!leftBracket.equals("(")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return bracketStack.isEmpty();
     }
 }
